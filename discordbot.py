077026d8cd0f6970438d29832f89a87906c37029
Paul Chua <paul.chua96@gmail.com>
2024-12-28 13:04:22 -0500

    Updated cleanup function

---
+++ discordbot.py
@@ -0,0 +1,50 @@
+import os
+import discord
+from random import randrange
+from discord import app_commands
+
+from scraper import *
+from dotenv import load_dotenv
+
+# get environment variables from .env file
+load_dotenv()
+
+client = discord.Client(intents = discord.Intents.all())
+tree = app_commands.CommandTree(client)
+discord_id = os.getenv('discord_id')
+
+@client.event
+async def on_ready():
+    channelid = os.getenv('channelid')
+    await tree.sync()
+    await client.get_channel(int(channelid)).send("Hello, bot is online now")
+    
+    print(f'{client.user} is now online!')
+
+
+@tree.command(name = "move", description="Learn basic information about a certain move")
+@app_commands.describe(move = "attack to learn information about")
+async def moveData(phrase: discord.Interaction, move: str):    
+    await phrase.response.send_message(getMoveData(move))
+    
+@tree.command(name = "ability", description="Learn what an ability does")
+@app_commands.describe(ability = "ability to learn information about")
+async def abilityData(phrase: discord.Interaction, ability: str):    
+    await phrase.response.send_message(getAbilityData(ability))
+    
+@tree.command(name = "pokemon", description="Learn the competitive relevant information about a Pokemon")
+@app_commands.describe(pokemon = "Pokemon to learn information about")
+async def PokemonData(phrase: discord.Interaction, pokemon:str):    
+    await phrase.response.send_message(getPokemonData(pokemon))
+
+@tree.command(name = "item", description="Learn what an item does")
+@app_commands.describe(item = "item to learn information about")
+async def PokemonData(phrase: discord.Interaction, item:str):    
+    await phrase.response.send_message(getItemData(item))
+
+@tree.command(name = "code", description= "Generate link code to use when playing vs others")
+async def randomgenerate(interaction: discord.Interaction):
+    await interaction.response.send_message(genCode())
+    
+
+client.run(os.getenv('TOKEN'))
---
+++ scraper.py
@@ -0,0 +1,126 @@
+import requests
+import bs4
+from random import randrange
+import json
+import re
+
+
+rootUrl = "https://pokemondb.net/move/"
+abilityUrl = "https://pokemondb.net/ability/"
+itemUrl = "https://pokemondb.net/item/"
+movesUrl = "https://play.pokemonshowdown.com/data/moves.json"
+
+ShowdownUrl = "https://play.pokemonshowdown.com/data/pokedex.json"
+
+def getPokemonData(text):
+        #Alternate forms are written like this: ninetalesalola, typenull, mimejr
+        htmlData = requests.get(ShowdownUrl)
+        monjson = json.loads(htmlData.content.decode('utf-8'))
+        mon = text.lower()
+        mon_info = ''
+        
+        if mon in monjson:
+               info = monjson[mon]
+               ability_list = []
+               mon_info += "Displaying data for: " + info['name'] + "\n"
+               mon_info += "Type: " + str(info['types']) + "\n"
+               # The data source has it as 0: Ability name ,just removing the number since its not relevant
+               for ability in info['abilities']:
+                       ability_list.append(info['abilities'][ability])
+                
+               mon_info += "Abilities: " + str(ability_list) + "\n"
+               
+               mon_info += "Base Stats: " + str(info['baseStats'])
+               return mon_info
+        else:
+             return "Pokemon not found, please check your spelling and try again"  
+       
+                       
+               
+
+def textCleanUp(text):
+        text = text.lower()
+        text = re.match("^[a-z\-0-9_ ]*$", text)
+        text = text.string
+        text = text.replace(' ','-')
+        return text
+        
+
+def getMoveData(attack):
+        htmldata = requests.get(movesUrl)
+        moveJson = json.loads(htmldata.content.decode('utf-8'))
+        fattack = "".join(attack.split())
+        fattack = fattack.lower()
+        
+        
+        if fattack in moveJson:
+                info = moveJson[fattack]
+                result = ""
+                result += "Displaying data for: " + attack + "\n"
+                result += "Type: " + str(info['type']) + "\n"
+                result += "Accuracy: " + str(info['accuracy']) + "\n"
+                result += "Category: " + str(info['category']) + "\n"
+                result += "Base Power: " + str(info['basePower']) + "\n"
+                result += "priority: " + str(info['priority']) + "\n"
+                result += "Description: " + info['shortDesc']
+                return result
+        return "No attack found, please try a different attack"
+        
+        
+                
+       
+def getAbilityData(ability):
+        
+       try:
+                data = "" 
+                ability = textCleanUp(ability)
+                htmlData = requests.get(abilityUrl+ability)
+                soup = bs4.BeautifulSoup(htmlData.text, "lxml")
+                
+                title = soup.find('h2').text
+                effect = soup.find('p').text
+                
+                data +=  title + ": " + effect
+                return data
+       except:
+               return "No data found"
+               
+      
+        
+        
+def getItemData(item):
+        try:
+                item = textCleanUp(item)
+                htmldata = requests.get(itemUrl + item);
+                soup = bs4.BeautifulSoup(htmldata.text,"lxml");
+                
+                #Removing this for now since its a little repetitive
+                #item_proper = soup.find('h1').text.strip();
+                effect = soup.find('p').text.strip();
+                return effect
+        
+        except:
+                return "No data found"
+
+
+def genCode():
+        result = randrange(99999999)
+        length = len(str(result))
+        if (length > 4):
+                test =  str(result)
+                testslice = slice(4)
+                
+                rest = test[4:]
+                list = [test[testslice], rest]
+                return " ".join(list)
+                
+                
+        
+        return result
+
+
+    
+    
+                
+        
+        
